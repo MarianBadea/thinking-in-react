c54718eb5f2b5d3c5d48291cb457669e49ac497d
MarainBadea <badea.alexandrumarian@gmail.com>
2021-07-19 15:00:07 +0300

    Group Products

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -7,10 +7,6 @@ function ProductTable (props) {
     const filterText = props.filterText;
     const inStockOnly = props.inStockOnly;
 
-    const rows = [];
-    let lastCategory = null;
-    
-
     let filteredProducts = props.products;
     if (filterText !== '') {
         filteredProducts = filterByName(filteredProducts, filterText);
@@ -19,22 +15,29 @@ function ProductTable (props) {
         filteredProducts = filterByStock(filteredProducts);
     }
 
-    filteredProducts.forEach((product) => {
-        if (product.category !== lastCategory) {
-            rows.push(
-                <ProductCategoryRow
-                category={product.category}
-                key={product.category} />
-            );
-        }
-        rows.push(
-            <ProductRow
-                product={product}
-                key={product.name}
-            />
-        );
-        lastCategory = product.category;
-    });    
+    const groupedProducts = groupByCategory(filteredProducts);
+    console.log(groupedProducts);
+    console.log(Object.entries(groupedProducts));
+    const rows = Object.entries(groupedProducts).map(([category, categoryProducts]) => {
+
+        const result = [];
+
+        result.push(
+            <ProductCategoryRow
+                category={category}
+                key={category} 
+            />);
+
+        categoryProducts.map(product => {
+            result.push(
+                <ProductRow
+                    product={product}
+                    key={product.name}
+                />)
+        });
+
+        return result;
+    });  
 
     return (
         <table className="product-table">
@@ -65,66 +68,16 @@ function filterByName (products, filterText) {
   
     return products.filter ( how );
   }
-    /*render() {
-      const filterText = this.props.filterText;
-      const inStockOnly = this.props.inStockOnly;
-  
-      const rows = [];
-      let lastCategory = null;
-  
-      let filteredProducts = this.props.products;
-      if (filterText !== '') {
-        filteredProducts = filterByName(filteredProducts, filterText);
-      }
-      if (inStockOnly) {
-        filteredProducts = filterByStock(filteredProducts);
-      }
-  
-      filteredProducts.forEach((product) => {
-        if (product.category !== lastCategory) {
-          rows.push(
-            <ProductCategoryRow
-              category={product.category}
-              key={product.category} />
-          );
-        }
-        rows.push(
-          <ProductRow
-            product={product}
-            key={product.name}
-          />
-        );
-        lastCategory = product.category;
-      });
-  
-      return (
-        <table className="product-table">
-          <thead>
-            <tr className="porduct-table-header product-table-row">
-              <th>Name</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      );
+
+  function groupByCategory(products) {
+    function getCategories(accumulator, product) {
+        let categoryProducts = accumulator[product.category] ? accumulator[product.category] : [];
+        categoryProducts.push(product);
+        accumulator[product.category] = categoryProducts;
+        return accumulator;
     }
+
+    return products.reduce(getCategories, {});
   }
-  
-  function filterByName (products, filterText) {
-    let how = (product) => {
-      return product.name.includes(filterText);
-    }
-  
-    return products.filter ( how )
-  }
-  
-  function filterByStock (products) {
-    let how = (product) => {
-      return product.stocked;
-    }
-  
-    return products.filter ( how );
-  }*/
 
   export default ProductTable;

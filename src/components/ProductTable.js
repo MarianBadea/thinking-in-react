72819161d95ddf14fc8a33c8ca8a44a868c68e7e
MarainBadea <badea.alexandrumarian@gmail.com>
2021-07-19 15:04:33 +0300

    Move filtering logic

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -2,22 +2,8 @@ import ProductRow from "./ProductRow";
 import ProductCategoryRow from "./ProductCategoryRow";
 
 
-function ProductTable (props) {
+function ProductTable ({groupedProducts}) {
 
-    const filterText = props.filterText;
-    const inStockOnly = props.inStockOnly;
-
-    let filteredProducts = props.products;
-    if (filterText !== '') {
-        filteredProducts = filterByName(filteredProducts, filterText);
-    }
-    if (inStockOnly) {
-        filteredProducts = filterByStock(filteredProducts);
-    }
-
-    const groupedProducts = groupByCategory(filteredProducts);
-    console.log(groupedProducts);
-    console.log(Object.entries(groupedProducts));
     const rows = Object.entries(groupedProducts).map(([category, categoryProducts]) => {
 
         const result = [];
@@ -51,33 +37,5 @@ function ProductTable (props) {
         </table>
       );
 }
-
-
-function filterByName (products, filterText) {
-    let how = (product) => {
-      return product.name.includes(filterText);
-    }
-  
-    return products.filter(how)
-  }
-
-  function filterByStock (products) {
-    let how = (product) => {
-      return product.stocked;
-    }
-  
-    return products.filter ( how );
-  }
-
-  function groupByCategory(products) {
-    function getCategories(accumulator, product) {
-        let categoryProducts = accumulator[product.category] ? accumulator[product.category] : [];
-        categoryProducts.push(product);
-        accumulator[product.category] = categoryProducts;
-        return accumulator;
-    }
-
-    return products.reduce(getCategories, {});
-  }
 
   export default ProductTable;
